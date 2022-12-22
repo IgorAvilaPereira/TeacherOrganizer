0bf91cd4b660cf38e254f69adf9757d3ac9cae15
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-12-22 14:55:42 -0300

    ok

--- lib/conexao.php
+++ lib/conexao.php
@@ -7,21 +7,21 @@ error_reporting(E_ALL);
 // ==================================================================
 // start na sessao
 session_start();
-if (!isset($_SESSION['login'])){
-    header("Location ./../bin/usurio/index.php");
-}
+// if (!isset($_SESSION['login'])){
+//     header("Location ./../bin/usurio/index.php");
+// }
 // ==================================================================
 // tamanho dos arquivos nos upload
 // sudo nano /etc/php/8.0/cli/php.ini
 // upload_max_filesize
 // ==================================================================
 define("URL", "http://localhost:8081/");
-define("DIR", "/home/iapereira/Dropbox/ifrs/docente-projeto-pessoal/docente/");
+define("DIR", "/home/iapereira/git/Teacher Organizer/");
 // ==================================================================
 // Permitir somente Minha Placa de Rede
-$resultado = shell_exec("ip -brief link");
+// $resultado = shell_exec("ip -brief link");
 // $resultado = shell_exec("ifconfig"); // tb funciona
-$resultado = explode(" ", $resultado);
+// $resultado = explode(" ", $resultado);
 // echo "<pre>";
 //     print_r($resultado[47]);
 //     print_r("<br>");
@@ -39,15 +39,15 @@ $resultado = explode(" ", $resultado);
 // }
 //aspire 5 e inspiron 5000
 // if(!(in_array("70:69:79:a7:cf:e2", $resultado) && in_array("20:1e:88:69:2d:32", $resultado)) && !(in_array("6c:2b:59:59:f2:39", $resultado) && in_array("04:ea:56:8e:3f:cd", $resultado))) {
-if(!(in_array("5c:f9:dd:5a:c5:fc", $resultado) && in_array("60:6c:66:24:fe:41", $resultado)) && !(in_array("70:69:79:a7:cf:e2", $resultado) && in_array("20:1e:88:69:2d:32", $resultado)) && !(in_array("6c:2b:59:59:f2:39", $resultado) && in_array("04:ea:56:8e:3f:cd", $resultado))) {
-    die("ACESSO NÃO PERMITIDO - NÃO É UM COMPUTADOR PERMITIDO.");
-}
+// if(!(in_array("5c:f9:dd:5a:c5:fc", $resultado) && in_array("60:6c:66:24:fe:41", $resultado)) && !(in_array("70:69:79:a7:cf:e2", $resultado) && in_array("20:1e:88:69:2d:32", $resultado)) && !(in_array("6c:2b:59:59:f2:39", $resultado) && in_array("04:ea:56:8e:3f:cd", $resultado))) {
+//     die("ACESSO NÃO PERMITIDO - NÃO É UM COMPUTADOR PERMITIDO.");
+// }
 // ==================================================================
 // Permitir Somente Acesso Local/Com o usuario iapereira
-$resultado = shell_exec("whoami");
-if (strcmp(explode("/", DIR)[2], trim($resultado)) !== 0 || strcmp($_SERVER["REMOTE_ADDR"], "127.0.0.1") != 0 || !(strpos($_SERVER["SCRIPT_FILENAME"], trim($resultado)) !== false) || !(strpos($_SERVER["DOCUMENT_ROOT"], trim($resultado)) !== false)) {
-    die("ACESSO NÃO PERMITIDO - NÃO É UMA CONTA LOCAL.");
-}
+// $resultado = shell_exec("whoami");
+// if (strcmp(explode("/", DIR)[2], trim($resultado)) !== 0 || strcmp($_SERVER["REMOTE_ADDR"], "127.0.0.1") != 0 || !(strpos($_SERVER["SCRIPT_FILENAME"], trim($resultado)) !== false) || !(strpos($_SERVER["DOCUMENT_ROOT"], trim($resultado)) !== false)) {
+//     die("ACESSO NÃO PERMITIDO - NÃO É UMA CONTA LOCAL.");
+// }
 //========================================================================
 // Banco - trocar senha padrao postgres
 // https://www.vivaolinux.com.br/dica/Alterando-senha-inicial-do-PostgreSQL-[Ubuntu]
