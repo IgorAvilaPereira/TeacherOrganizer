d6b34106fe3ff46b57a9846b7a49ba1b2584b59f
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-02-25 17:45:51 -0300

    excluir cursos

--- bin/cursos/excluir.php
+++ bin/cursos/excluir.php
@@ -1,12 +1,30 @@
 <?php
 	require_once "../../lib/conexao.php";
-	// DELETAR TODAS AS DISCIPLINAS DO CURSO ANTES
-	$query  = "delete from cursos where id = ".$_GET['id'];
 	
-	// $result =  pg_query($query) or die(pg_errormessage());
-	$result = pg_query_params($conexao, $query, array()) or die ($query);
-
-	
-	header("Location: index.php");
-	
+	$sql = "select * from disciplinas where curso_id = ".$_GET['id'].";";    
+    $result = pg_query_params($conexao, $sql, array()) or die ($sql);
+    
+    if (pg_affected_rows($result)>0) {
+        while ($registro = pg_fetch_array($result)) {	
+			$sql = "delete from presencas where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from anotacoes where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from creditos where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from planos where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from notas where aluno_id in (select id from alunos where disciplina_id = ".$registro['id'].");";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from avaliacoes where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from alunos where disciplina_id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+			$sql = "delete from disciplinas where id = ".$registro['id'].";";
+			$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+		}
+	}
+	$query  = "delete from cursos where id = ".$_GET['id'];	
+	$result = pg_query_params($conexao, $query, array()) or die ($query);	
+	header("Location: index.php");	
 ?>
--- bin/cursos/inserir.php
+++ bin/cursos/inserir.php
@@ -3,7 +3,9 @@
 	
 	$query  = "insert into cursos (nome) values ('".$_POST['nome']."');";
 	
-	$result =  pg_query($query);
+	// $result =  pg_query($query);
+	$result = pg_query_params($conexao, $query, array()) or die ($query);
+
 	
 	header("Location: index.php");
 	
