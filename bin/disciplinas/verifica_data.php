71931974749d65a9566df6d60806149ffc7e9586
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-04-29 10:53:51 -0300

    pg

--- bin/disciplinas/verifica_data.php
+++ bin/disciplinas/verifica_data.php
@@ -2,7 +2,7 @@
 	require_once "../../lib/conexao.php";
 
 	$query = "select * FROM creditos where disciplina_id = ".$_GET['id_disciplina']." and dia_semana=".$_GET['dia_semana'];
-	$result = pg_query($query);
+	$result = pg_query_params($conexao, $query, array()) or die ($query);
 	echo pg_affected_rows($result);
 	
 ?>
