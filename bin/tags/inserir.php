42c563b16442efe8338f53ac11753812591d50da
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-05-01 14:01:23 -0300

    params 1

--- bin/tags/inserir.php
+++ bin/tags/inserir.php
@@ -1,9 +1,9 @@
 <?php
 	require_once "../../lib/conexao.php";	
 	
-	$sql = "INSERT INTO tags (tag) values ('".$_POST['tag']."');";	
+	$sql = "INSERT INTO tags (tag) values ($1);";	
 	// $result = pg_query($sql) or die ($sql);	
-	$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+	$result = pg_query_params($conexao, $sql, array($_POST['tag'])) or die ($sql);
 
 
 	header("Location: index.php");
--- bin/tags/tela_alterar.php
+++ bin/tags/tela_alterar.php
@@ -5,9 +5,9 @@
 	
 	$template = new Template("../../view/tags/tela_alterar.html");
 	
-	$sql = "select * from tags WHERE id = ".$_GET['id'];
+	$sql = "select * from tags WHERE id = $1";
 	// $result = pg_query($sql) or die($sql);	
-	$result = pg_query_params($conexao, $sql, array()) or die ($sql);
+	$result = pg_query_params($conexao, $sql, array($_GET['id'])) or die ($sql);
 
 	$registro = pg_fetch_array($result);
 	$template->tag = nl2br($registro['tag']);
