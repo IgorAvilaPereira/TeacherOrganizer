f8b0fc083b5af6a664269fcb7487c3c389c08a84
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-05-01 13:53:49 -0300

    params 1

--- bin/planos/inserir.php
+++ bin/planos/inserir.php
@@ -5,12 +5,12 @@
 	
 		$query = "INSERT INTO planos(bimestre, data, texto, disciplina_id)
     	VALUES (
-    		".$_POST['bimestre'].",
-    		'".$_POST['data']."', 
-    		'".trim($_POST['texto'])."', 
-    		".$_POST['id_disciplina'].");";
+    		$1,
+    		$2,
+    		$3,
+    		$4);";
 		// $result = pg_query($sql) or die($sql);
-		$result = pg_query_params($conexao, $query, array()) or die ($query);
+		$result = pg_query_params($conexao, $query, array($_POST['bimestre'], $_POST['data'], trim($_POST['texto']), $_POST['id_disciplina'])) or die ($query);
 
 		
 		dump(); // realiza o dump com as presencas/ausencias do dia mais o plano recem cadastrado
